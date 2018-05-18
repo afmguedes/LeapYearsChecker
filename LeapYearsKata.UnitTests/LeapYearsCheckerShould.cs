50ad0c3f5a5d769b6a23ab52f3e561376550ddcb
Andre Guedes <andre.guedes@asos.com>
2018-05-18 16:18:32 +0100

    First unit test

---
+++ LeapYearsKata.UnitTests/LeapYearsCheckerShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeapYearsKata.UnitTests
+{
+    [TestFixture]
+    public class LeapYearsCheckerShould
+    {
+        [Test]
+        public void ReturnFalse_WhenCheckIsCalledWithEmptyString()
+        {
+            var input = string.Empty;
+            var leapYearsChecker = new LeapYearsChecker();
+
+            var result = leapYearsChecker.Check(input);
+
+            result.Should().BeFalse();
+        }
+    }
+
+    public class LeapYearsChecker
+    {
+        public bool Check(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
